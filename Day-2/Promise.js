eb8510917c8481946e7fa915cda7b60091896064
Balkrishna Tripathi <Balkrishna@softura.com>
2021-11-02 17:01:46 +0530

    Promise modified

---
+++ Day-2/Promise.js
@@ -0,0 +1,20 @@
+function IDontHaveItNow(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            return "Got It !!!!!!    Take IT !!!!!!"
+        },3000);
+    });
+    
+}
+caller=IDontHaveItNow();
+
+caller.then(
+    (result)=> console.log(result),
+    (error)=> console.log(error)
+)
+
+//console.log(caller)
+
+//the function gets the value from a source asynchronously
+//not immediatly there will be delay in getting the data from source
+//Javascript is asynchronous in nature
